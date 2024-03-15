0b7a2fc6d9e864f2241c7645b250ae16da4b34bf
erhancicek <163591103+erhancicek@users.noreply.github.com>
2024-03-15 23:06:10 +0300

    Update flappy_bird.py

--- flappyBird_pygame/flappy_bird.py
+++ flappyBird_pygame/flappy_bird.py
@@ -273,3 +273,4 @@ if __name__ == "__main__":
     # If this module had been imported, __name__ would be 'flappybird'.
     # It was executed (e.g. by double-clicking the file), so call main.
     main()
+    input
